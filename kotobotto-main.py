65ad88879746d4c0a5e5875d49c649e4031b05aa
Thomas Trivino <th992984@ucf.edu>
2024-09-16 23:10:21 -0400

    added support for env variables

--- kotobotto-main.py
+++ kotobotto-main.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+
+load_dotenv()
 
 token = os.getenv('DISCORD_BOT_TOKEN')
 
@@ -12,8 +15,8 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
+async def printUserInfo(ctx, user: discord.User):
+    await ctx.send(f'User ID: {user.id}\nUsername: {user.name}\nDiscriminator: {user.discriminator}')
 
 
 bot.run(token)
