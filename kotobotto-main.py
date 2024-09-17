a393fd8f05d785872b6ebcff5ec8cccc37ac5fae
Thomas Trivino <th992984@ucf.edu>
2024-09-16 22:44:25 -0400

    Added token to env variable

--- kotobotto-main.py
+++ kotobotto-main.py
@@ -1,5 +1,8 @@
+import os
 import discord
 from discord.ext import commands
+
+token = os.getenv('DISCORD_BOT_TOKEN')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -13,4 +16,4 @@ async def add(ctx, a: int, b: int):
     await ctx.send(a + b)
 
 
-bot.run('TOKEN')
+bot.run(token)
